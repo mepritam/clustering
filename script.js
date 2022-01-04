468172f0e44b89794d70969914ab8109fefa3369
Pritam S <me@pritamsadhu.in>
2022-01-04 21:05:38 +0530

    XX-X: Added sector wrapper hide functionality.

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
+var firstForm = document.getElementById("box");
+var secondForm = document.querySelectorAll("#second");
+var sectorWrapper = document.getElementById("sector-wrapper");
+var box = document.getElementById("box");
+var box2 = document.getElementById("box2");
+var sectorWrapper = document.getElementById("sector-wrapper");
+// default sector wrapper is hidden 
+sectorWrapper.style.display = "none";
 
-
-
-var firstForm=document.getElementById("box");
-var secondForm=document.querySelectorAll("#second");
-
-var box2=document.getElementById("box2");
-
-var FI=document.getElementById("FI");
-FI.addEventListener("click",()=>{
-
-    firstForm.style.visibility= "visible";
-    secondForm[0].style.visibility= "visible";
-    secondForm[1].style.visibility= "visible";
-    box2.style.display="none";
-    firstForm.style.color="red";
-    console.log("red");
+document.getElementById("FI").addEventListener("click", () => {
+    // hide sector wrapper 
+    sectorWrapper.style.display = "none";
+    box.style.display = "block";
+    box2.style.display = "none";
 })
 
-var NFI=document.getElementById("NFI");
-
-NFI.addEventListener("click",()=>{
-
-firstForm.style.visibility="hidden";
-box2.style.visibility="visible";
-secondForm[0].style.visibility="hidden";
-secondForm[1].style.visibility="visible";
-    
-    })
+document.getElementById("NFI").addEventListener("click", () => {
+    // enable sector wrapper 
+    sectorWrapper.style.display = "block";
+    box.style.display = "none";
+    box2.style.display = "block";
+})
